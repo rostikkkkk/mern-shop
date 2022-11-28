d38e6d3bbf84a7ecd6386480f3aef130b3630ff6
Rostislav Martinyuk <kievstarpro0108201@gmail.com>
2022-11-28 04:59:52 +0200

    react create app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazon</a>
+      </header>
+      <main>
+        items
+      </main>
+    </div>
+  );
+}
+
+export default App;
